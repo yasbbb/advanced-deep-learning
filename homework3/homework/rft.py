62c90218347b8514569e6b9ce88c0c3b0e0377b2
Yash Bardhan <yash.bardhan09@gmail.com>
2026-04-03 20:27:05 -0500

    hw3

---
+++ homework3/homework/rft.py
@@ -0,0 +1,31 @@
+from .base_llm import BaseLLM
+from .sft import test_model
+
+
+def load() -> BaseLLM:
+    from pathlib import Path
+
+    from peft import PeftModel
+
+    model_name = "rft_model"
+    model_path = Path(__file__).parent / model_name
+
+    llm = BaseLLM()
+    llm.model = PeftModel.from_pretrained(llm.model, model_path).to(llm.device)
+    llm.model.eval()
+
+    return llm
+
+
+def train_model(
+    output_dir: str,
+    **kwargs,
+):
+    # Reuse much of the SFT code here
+    raise NotImplementedError()
+
+
+if __name__ == "__main__":
+    from fire import Fire
+
+    Fire({"train": train_model, "test": test_model, "load": load})
